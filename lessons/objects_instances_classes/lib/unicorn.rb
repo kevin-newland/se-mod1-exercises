37bc9a286c0716ff3287ea68d567d0a992832c91
Kevin Newland <kevin.newland@yahoo.com>
2024-10-08 12:14:49 -0500

    unicorn class created

---
+++ lessons/objects_instances_classes/lib/unicorn.rb
@@ -0,0 +1,13 @@
+class Unicorn
+  attr_reader :name, :color, :magical_powers
+  def initialize(name, color)
+    @name = name
+    @color = color
+    @magical_powers = []
+  end
+
+  def add_power(power)
+    @magical_powers << power
+  end
+end
+
